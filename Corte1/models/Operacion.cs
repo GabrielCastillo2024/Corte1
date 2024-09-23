1eee9fe35ce492a7aa61532b998360c908331e0e
GabrielCastillo2024 <gecastilloc@uamv.edu.ni>
2024-09-23 10:17:25 -0600

    Creacion de clase Operaciones.cs

---
+++ Corte1/models/Operacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    public class Operacion
+    {
+        public int CalcularEdad(Persona persona)
+        {
+            int edad = DateTime.Now.Year - persona.FechaNacimiento.Year;
+            if (DateTime.Now.DayOfYear < persona.FechaNacimiento.DayOfYear)
+            {
+                edad--;
+            }
+            return edad;
+        }
+
+        public bool EsMayorDeEdad(Persona persona)
+        {
+            return CalcularEdad(persona) >= 18;
+        }
+    }
+}
