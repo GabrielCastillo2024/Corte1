4a4db825d4315ffe2c13990febe47d094e9b7174
GabrielCastillo2024 <gecastilloc@uamv.edu.ni>
2024-09-23 10:14:25 -0600

    Creacion de clase Registro.cs

---
+++ Corte1/models/Registro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corte1.models
+{
+    public class Registro
+    {
+        private Persona[] personas;
+        private int contador;
+
+        public Registro()
+        {
+            personas = new Persona[30];
+            contador = 0;
+        }
+
+        public void AgregarPersona(Persona persona)
+        {
+            if (contador < 30)
+            {
+                personas[contador] = persona;
+                contador++;
+            }
+            else
+            {
+                throw new InvalidOperationException("El registro está lleno.");
+            }
+        }
+
+        public Persona[] ObtenerPersonas()
+        {
+            return personas.Take(contador).ToArray();
+        }
+    }
+}
+
